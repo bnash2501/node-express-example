e81ed4793e5faff9852ae79d8d9f5a3448b7ac28
Brian <bnash@phase2technology.com>
2017-10-03 11:11:12 -0400

    Simple server.

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  var headers = JSON.stringify(req.headers);
+  var params = JSON.stringify(req.params);
+  var query = JSON.stringify(req.query);
+  var cookies = JSON.stringify(req.cookies);
+  var signedCookies = JSON.stringify(req.signedCookies);
+
+  console.log(`current cookies in request: ${cookies}`)
+  var randomNumber=Math.random().toString();
+  randomNumber=randomNumber.substring(2,randomNumber.length);
+  res.cookie('cokkieName',randomNumber, { maxAge: 900000, httpOnly: true })
+  console.log('cookie have created successfully');
+
+  res.render('index', {
+    title: 'Express',
+    request : {
+      headers: headers,
+      params: params,
+      query: query,
+      cookies: cookies,
+      signedCookies: signedCookies
+    }
+  });
+});
+
+module.exports = router;
